f5aad81e1211ee620a45cfbfaddefdca8d6c55b4
Danilo Josino <danilo@josino.com.br>
2022-10-06 09:43:40 -0300

    att

--- EXERCÍCIO_01.py
+++ EXERCÍCIO_01.py
@@ -7,16 +7,25 @@ if  emprego == 'Sim':
     renda = input('Salário: ')
 else:
     renda = 0
+    print('NÃO Elegível')
+    exit
+
 estado_civil = input('Estado Civil: ')
 filhos = input('Tem filhos? ')
 if filhos == "Sim":
     input('Quantos? ')
 idade = 2022 - int(nascimento)
+
+if idade < 18 || idade > 40:
+  print('NÃO Elegível')
+  exit
+
 margem_minima = 1200
 margem_maior = 4000
 renda = int(renda)
 
-if idade >= 18 and idade <= 40 and emprego == 'Sim' and renda > margem_minima:
+if renda > margem_minima:
     print('Elegível')
 else:
     print('NÃO Elegível')
+
