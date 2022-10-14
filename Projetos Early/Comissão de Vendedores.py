5661020db31557d1e69750f543a1f71708f85040
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2022-10-14 13:04:20 -0300

    Update Comissão de Vendedores.py

--- Projetos Early/Comissão de Vendedores.py
+++ Projetos Early/Comissão de Vendedores.py
@@ -3,20 +3,28 @@ print()
 
 meta_loja = 50000
 taxa_comissao = ''
-ifood = 8022,60
-rappi = 205,80
+total_loja = []
+y = 0
 
 vendedora = input('Vendedor(a): ')
 loja = input('Loja: ')
-venda = input('Valor da Venda: ')
 
-if float(venda) <= 25000:
+while True:
+    venda = input('Valor da Venda: ')
+    if not venda.isalpha():
+        venda = float(venda)
+        break
+    else:
+        print('APENAS NÚMEROS!')
+
+if venda <= 25000:
     taxa_comissao = 0.007
-elif float(venda) >= 25000 and float(venda) <= 35000:
+elif venda >= 25000 and venda <= 35000:
     taxa_comissao = 0.011
-elif float(venda) >= 35000 and float(venda) <= 50000:
+elif venda >= 35000 and venda <= 50000:
     taxa_comissao = 0.025
-elif float(venda) > 50000:
+elif venda > 50000:
     taxa_comissao = 0.03
 
-print(f'A comissão da {vendedora} é de R$ {float(venda) * float(taxa_comissao):.2f}')
+
+print(f'A comissão da {vendedora} é de R$ {venda * float(taxa_comissao):.2f}')
