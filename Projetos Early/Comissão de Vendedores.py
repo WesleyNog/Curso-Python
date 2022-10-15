925641acb79025862ca4a61712c8b1c2999c0835
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2022-10-15 11:19:10 -0300

    Update Comissão de Vendedores.py
    O loop começou a funcionar, próximo passo e fazer o somatório total do loop!

--- Projetos Early/Comissão de Vendedores.py
+++ Projetos Early/Comissão de Vendedores.py
@@ -3,36 +3,23 @@ print()
 
 meta_loja = 50000
 taxa_comissao = ''
-total_loja = []
 y = 0
 
 qnt_vendedora = int(input('Quantas vendedoras? '))
-while True:
-    if qnt_vendedora >= y:
-        loja = input('Loja: ')
-        vendedora = input('Vendedor(a): ')
-        venda = input('Valor da Venda: ')
-        if not venda.isalpha():
-            venda = float(venda)
-        else:
-            print('APENAS NÚMEROS!')
-            continue
-        print(f'A comissão da {vendedora} é de R$ {venda * float(taxa_comissao):.2f}')
-    else:
-
-    y += 1
-
-    
-    if venda <= 25000.0:
+while y < qnt_vendedora:
+    loja = input('Loja: ')
+    vendedora = input('Vendedor(a): ')
+    venda = input('Valor da Venda: ')
+    venda = float(venda)
+    if venda <= 25000:
         taxa_comissao = 0.007
-    elif venda >= 25000.0 and venda <= 35000.0:
+    elif venda >= 25000 and venda <= 35000:
         taxa_comissao = 0.011
-    elif venda >= 35000.0 and venda <= 50000.0:
+    elif venda >= 35000 and venda <= 50000:
         taxa_comissao = 0.025
-    else:
+    elif venda > 50000:
         taxa_comissao = 0.03
-
-    print(f'{venda}')
-    print(f'{taxa_comissao}')
-    print(f'O total de vendas é de R$ {venda * float(taxa_comissao):.2f}')
+    print(f'A comissão da {vendedora} é de R$ {venda * float(taxa_comissao):.2f}')
+    y += 1
     
+print('Fim!')
