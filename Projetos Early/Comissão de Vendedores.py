bb533eb411d8f391332ced18df6547b6018d48c3
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2022-10-14 21:25:04 -0300

    Update Comissão de Vendedores.py

--- Projetos Early/Comissão de Vendedores.py
+++ Projetos Early/Comissão de Vendedores.py
@@ -6,25 +6,28 @@ taxa_comissao = ''
 total_loja = []
 y = 0
 
-vendedora = input('Vendedor(a): ')
-loja = input('Loja: ')
-
+qnt_vendedora = int(input('Quantas vendedoras? '))
 while True:
-    venda = input('Valor da Venda: ')
-    if not venda.isalpha():
-        venda = float(venda)
-        break
+    if qnt_vendedora >= y:
+        loja = input('Loja: ')
+        vendedora = input('Vendedor(a): ')
+        venda = input('Valor da Venda: ')
+        if not venda.isalpha():
+            venda = float(venda)
+            break
+        else:
+            print('APENAS NÚMEROS!')
+        print(f'A comissão da {vendedora} é de R$ {venda * float(taxa_comissao):.2f}')
     else:
-        print('APENAS NÚMEROS!')
+        print(f'O total de vendas é de R$ {venda * float(taxa_comissao):.2f}')
+    y += 1
 
-if venda <= 25000:
-    taxa_comissao = 0.007
-elif venda >= 25000 and venda <= 35000:
-    taxa_comissao = 0.011
-elif venda >= 35000 and venda <= 50000:
-    taxa_comissao = 0.025
-elif venda > 50000:
-    taxa_comissao = 0.03
-
-
-print(f'A comissão da {vendedora} é de R$ {venda * float(taxa_comissao):.2f}')
+    
+    if venda <= 25000:
+        taxa_comissao = 0.007
+    elif venda >= 25000 and venda <= 35000:
+        taxa_comissao = 0.011
+    elif venda >= 35000 and venda <= 50000:
+        taxa_comissao = 0.025
+    elif venda > 50000:
+        taxa_comissao = 0.03
