ddb10528337a304d0de41421d3caec4134761291
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2022-10-25 23:13:55 -0300

    Create Dados p Aulas 23_24_25.py

---
+++ Aulas/Dados p Aulas 23_24_25.py
@@ -0,0 +1,29 @@
+# Aula de MAP
+
+produtos = [
+    {'nome': 'p1', 'preco': 22.50},
+    {'nome': 'p2', 'preco': 33.25},
+    {'nome': 'p3', 'preco': 12.48},
+    {'nome': 'p4', 'preco': 45.30},
+    {'nome': 'p5', 'preco': 50.00},
+    {'nome': 'p6', 'preco': 72.15},
+    {'nome': 'p7', 'preco': 10.50},
+    {'nome': 'p8', 'preco': 7.20},
+    {'nome': 'p9', 'preco': 20.15},
+    {'nome': 'p10', 'preco': 22.50},
+]
+
+pessoas = [
+    {'nome': 'wesley', 'idade': 35},
+    {'nome': 'naiara', 'idade': 28},
+    {'nome': 'helloa', 'idade': 9},
+    {'nome': 'eurides', 'idade': 56},
+    {'nome': 'valnides', 'idade': 59},
+    {'nome': 'valter', 'idade': 37},
+    {'nome': 'valjunior', 'idade': 32},
+    {'nome': 'jamile', 'idade': 29},
+    {'nome': 'rute', 'idade': 50},
+    {'nome': 'helane', 'idade': 33},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
