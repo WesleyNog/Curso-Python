f7cc15c6134ffe04b6cee1407d26948d97455b58
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2022-11-30 14:52:42 -0300

    Update bot_izzy.py
    ...

--- Projetos Early/Bot IzzyWay/bot_izzy.py
+++ Projetos Early/Bot IzzyWay/bot_izzy.py
@@ -1,6 +1,6 @@
 import func
 
-func.logar('adm4@briejer.com.br', '225368')
+func.logar('####@####.com.br', '####')
 func.navegar('compra', 'materia', 'Compras')
 func.mid('rose batista', 'castanha terce', '271122', '15')
 func.pagamento('deposito', '30112022')
