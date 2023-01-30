bce03b339003867e9ccf102d12bf28f8b9232e85
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2023-01-30 18:00:45 -0300

    Large Update
    Maior segmentação dos métodos

--- Projetos Early/bot_izzyway_financeiro/class_robot.py
+++ Projetos Early/bot_izzyway_financeiro/class_robot.py
@@ -7,7 +7,6 @@ from xpath import modulos_xpaths, lojas_xpaths, cp_xpaths;
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-from datetime import datetime
 
 
 option = webdriver.FirefoxOptions()
@@ -54,119 +53,66 @@ class RoboIzzyWay:
 
         # Acessar o Contas a Pagar
         access_cp = self.driver.find_element(By.XPATH, cp_xpaths['contas_pagar']).click()
-        ##search_cp = self.driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[2]/div[1]/div/div[1]/div[1]/h2').text
-
-        ##if search_cp != 'Contas a Pagar':
-        ##    access_cp = self.driver.find_element(By.XPATH, cp_xpaths['contas_pagar']).click()
         sleep(10)
     
     #############TESTANDO MÉDOTO################
     def find_xpath(self, xpath):
-        try:
-            return self.driver.find_element(By.XPATH, xpath)
-        except:
-            print('ERROR: Elemento não encontrado! Retomando...')
-            sleep(10)
-            return self.driver.find_element(By.XPATH, xpath)
+        return WebDriverWait(self.driver, 30).until(
+                    EC.presence_of_element_located((By.XPATH, xpath))
+                )
 
 
     # Acessar a Loja que ira ser feita a operação
-    def lancamento(self):
-        
-        count = 0
-        search_loja = self.driver.find_element(By.XPATH, '//*[@id="lblEstabelecimentoSelecionado"]').text
-        # LOOP
-        for i, row in LER.iterrows():
-            if search_loja != str(row['LOJA']).upper():
-                lj01 = WebDriverWait(self.driver, 30).until(
-                    EC.presence_of_element_located((By.XPATH, lojas_xpaths['loja']))
-                )
-                lj01.click()
-
-                sleep(0.5)
-
-                lj02 = WebDriverWait(self.driver, 30).until(
-                    EC.presence_of_element_located((By.XPATH, lojas_xpaths[str(row['LOJA']).lower()]))
-                )
-                lj02.click()
-                
-                sleep(10)
-            try:
-                # Adicionar um novo LANCAMENTO do CONTAS A PAGAR
-                novo = WebDriverWait(self.driver, 30).until(
-                    EC.presence_of_element_located((By.XPATH, cp_xpaths['novo_cp']))
-                )
-                novo.click()
-            except TimeoutError:
-                sleep(10)
-                novo = WebDriverWait(self.driver, 30).until(
-                    EC.presence_of_element_located((By.XPATH, cp_xpaths['novo_cp']))
-                )
-                novo.click()
-
-            # Escolha de Lancamento Simples
-            simples = WebDriverWait(self.driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, cp_xpaths['lancamento_simples']))
-            )
-            simples.click()
-            
-            sleep(2)
-
-            # Lançamentos em Loop da Planilha em Excel  cp_xpaths['plano_contas']
-          
-            plano_contas = self.find_xpath(cp_xpaths['plano_contas']).send_keys(row['PLANO'], Keys.ENTER)
-            centro_resultados = self.find_xpath(cp_xpaths['centro_resultados']).send_keys('1', Keys.ENTER)
-            documento = self.find_xpath(cp_xpaths['documento']).send_keys(row['DOCUMENTO'], Keys.ENTER)
-            participante = self.find_xpath(cp_xpaths['participante']).send_keys(row['PARTICIPANTE'], Keys.ENTER)
-            sleep(1)
-            emissao = self.find_xpath(cp_xpaths['emissao'])
-            emissao.click()
-            emissao.clear()
-            emissao.send_keys(str(row['EMISSAO']))
-            vencimento = self.find_xpath(cp_xpaths['vencimento'])
-            vencimento.click()
-            vencimento.clear()
-            vencimento.send_keys(str(row['VENCIMENTO']))
-            valor = self.find_xpath(cp_xpaths['valor']).send_keys(row['VALOR'], Keys.ENTER)
-            historico = self.find_xpath(cp_xpaths['historico']).send_keys(row['HISTORICO'], Keys.ENTER)
-            forma_pagamento = self.find_xpath(cp_xpaths['forma_pagamento'])
-            forma_pagamento.click()
-            sleep(0.5)
-            select_forma_pagamento = self.find_xpath(cp_xpaths['lista_pagamento'].replace('$$NUMBER$$', str(row['FORMA DE PAGAMENTO'])))
-            select_forma_pagamento.click()
-            data_pagamento = self.find_xpath(cp_xpaths['data_pagamento']).send_keys(str(row['DATA DE PAGAMENTO']))
-            salvar = self.find_xpath(cp_xpaths['salvar']).click()
-            sleep(3)
-            
-
-            ###carregando = self.driver.find_element(By.XPATH, '/html/body/div[5]/h2/h3')
-            ###campos_obrigatorios = self.driver.find_element(By.XPATH, '<div class="toast-message">Campo Participante obrigatório</div>')
-            ###savo_sucesso = self.driver.find_element(By.XPATH, '<div class="toast-message">Salvo com sucesso</div>')
-            
-            current_date = datetime.now()
-            search_loja = self.find_xpath('//*[@id="lblEstabelecimentoSelecionado"]').text
-            count += 1
-
-            if count == (i + 1):
-                print(f'{str(count) + "º"} LANCAMENTO ✅ | Nº Doc {row["DOCUMENTO"]}')
-                print(f'Lançado na loja do {search_loja}.')
-                with open(LOG, 'a+') as arquivo:
-                    arquivo.write(f'{str(count) + "º"} LANCAMENTO OK! | Nº Doc {row["DOCUMENTO"]}\n')
-                    arquivo.write(f'Lançado na loja do {search_loja}.\n')
-                    arquivo.write(f'{current_date}\n')
-                    arquivo.write('\n')
-                    arquivo.write('-' * 20)
-                    arquivo.write('\n')
-            else:
-                print(f'ERROR: {str(i) + "º"} LANCAMENTO não realizado! ❌')
-                arquivo.write(f'ERROR: {str(i) + "º"} LANCAMENTO não realizado!\n')
-
+    def select_store(self, store):
+        search_loja = self.find_xpath('//*[@id="lblEstabelecimentoSelecionado"]').text
+        if search_loja != str(store).upper():
             sleep(5)
+            lj01 = self.find_xpath(lojas_xpaths['loja']).click()
 
-            #if self.driver.find_element(By.XPATH, '/html/body/div[5]/h2/h3'):
-            #    sleep(1)
+            sleep(0.5)
 
+            lj02 = self.find_xpath(lojas_xpaths[str(store).lower()]).click()
+            
+            sleep(10)
+
+    def new_insert(self):        
+        try:
+            # Adicionar um novo LANCAMENTO do CONTAS A PAGAR
+            novo = self.find_xpath(cp_xpaths['novo_cp']).click()
+        except TimeoutError:
+            sleep(10)
+            novo = self.find_xpath(cp_xpaths['novo_cp']).click()
+
+        # Escolha de Lancamento Simples
+        simples = self.find_xpath(cp_xpaths['lancamento_simples']).click()
         
-        print('-' * 20)
-        print()
-        print(f'O total {count} de {self.qnt_lancamento} LANCAMENTOS foram REALIZADOS!')
+        sleep(2)
+
+
+    def fill_in_release(self, plano, documento, participante, data_emissao, data_vencimento, valor, historico, forma_pgto, data_pgto):
+        # Lançamentos em Loop da Planilha em Excel  cp_xpaths['plano_contas']
+        
+        plano_contas = self.find_xpath(cp_xpaths['plano_contas']).send_keys(plano, Keys.ENTER)
+        centro_resultados = self.find_xpath(cp_xpaths['centro_resultados']).send_keys('1', Keys.ENTER)
+        documento = self.find_xpath(cp_xpaths['documento']).send_keys(documento, Keys.ENTER)
+        participante = self.find_xpath(cp_xpaths['participante']).send_keys(participante, Keys.ENTER)
+        sleep(1)
+        emissao = self.find_xpath(cp_xpaths['emissao'])
+        emissao.click()
+        emissao.clear()
+        emissao.send_keys(str(data_emissao), Keys.ENTER)
+        vencimento = self.find_xpath(cp_xpaths['vencimento'])
+        vencimento.click()
+        vencimento.clear()
+        vencimento.send_keys(str(data_vencimento), Keys.ENTER)
+        valor = self.find_xpath(cp_xpaths['valor']).send_keys(valor, Keys.ENTER)
+        historico = self.find_xpath(cp_xpaths['historico']).send_keys(historico, Keys.ENTER)
+        forma_pagamento = self.find_xpath(cp_xpaths['forma_pagamento'])
+        forma_pagamento.click()
+        sleep(0.5)
+        select_forma_pagamento = self.find_xpath(cp_xpaths['lista_pagamento'].replace('$$NUMBER$$', str(forma_pgto)))
+        select_forma_pagamento.click()
+        data_pagamento = self.find_xpath(cp_xpaths['data_pagamento']).send_keys(str(data_pgto), Keys.ENTER)
+        salvar = self.find_xpath(cp_xpaths['salvar']).click()
+        sleep(3)
+
