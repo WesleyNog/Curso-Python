ab2e2c031e53e6fe987296edea6ae2fab0f58ee1
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2023-02-03 11:56:32 -0300

    Delete Projetos Early/Bot IzzyWay directory

--- Projetos Early/Bot IzzyWay/bot_izzy.py
+++
@@ -1,7 +0,0 @@
-import func
-
-func.logar('####@####.com.br', '####')
-func.navegar('compra', 'materia', 'Compras')
-func.mid('rose batista', 'castanha terce', '271122', '15')
-func.pagamento('deposito', '30112022')
-func.plano_contas('4.01.01')
