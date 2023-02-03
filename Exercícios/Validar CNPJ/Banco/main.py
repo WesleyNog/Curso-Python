70bf89c815cb89887c02e487defd1d14a93a55e8
WesleyNog <115157703+WesleyNog@users.noreply.github.com>
2023-02-02 22:10:37 -0300

    Início do exercócio orientado de Clases

---
+++ Exercícios/Validar CNPJ/Banco/main.py
@@ -0,0 +1,31 @@
+import conta as C
+
+# Deve ter limite Extras
+# Deve ter métodos de Depositos
+class SavingsAccount(C.Accounts):
+        def _account(self, account):
+            self.account = account
+        
+
+        def deposito(self):
+            ...
+
+# Deve ter métodos de Depositos
+class CurrentAccount(C.Accounts):
+        def _account(self, account):
+            self.account = account
+        
+
+        def deposito(self):
+            ...
+
+
+
+
+
+# Metodo para Autenticar Contas e Clientes
+# Agregação de Contas e Clientes
+class Bank:
+    ...
+
+
---
+++ Exercícios/Validar CNPJ/Banco/pessoas.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+# Outra Classe Abstata
+class People(ABC):
+    def _people(self, name, age):
+        ...
+
+
+class Client(People):
+    ...
